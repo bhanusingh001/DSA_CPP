6b53f0bfe112929a7d205eeaaffc36444e03e7cb
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-17 23:44:56 +0530

    ✅ Solved: Input and Output (I/O)

---
+++ 01_Basics_IO/002_Input_and_Output.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: Input and Output
+ * Description: ? Solve ?
+ * Topic: I/O
+ * Date: 2026-02-17 23:44
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    
+    return 0;
+}
