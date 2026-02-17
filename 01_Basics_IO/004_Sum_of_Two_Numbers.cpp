f19ae5b93506be1e040b75017b5516d2f448c3e2
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-18 00:05:07 +0530

    ✅ Solved: Sum of Two Numbers (I/O)

---
+++ 01_Basics_IO/004_Sum_of_Two_Numbers.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem: Sum of Two Numbers
+ * Description: Read two integers a, b. Print their sum, difference, product and quotient each on a new line.
+ * Topic: I/O
+ * Date: 2026-02-18 00:05
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+
+    if (b != 0) {
+        cout << a / b << endl;
+    } else {
+        cout << "Error: Division by zero" << endl;
+    }
+
+    return 0;
+}
