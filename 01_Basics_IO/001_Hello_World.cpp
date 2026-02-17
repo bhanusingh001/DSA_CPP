4bd178b3c9bfbb7c4a1257e2c37036360000c2cf
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-17 23:37:08 +0530

    ✅ Solved: Hello World (I/O)

---
+++ 01_Basics_IO/001_Hello_World.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem: Hello World
+ * Description: ? Solve ?
+ * Topic: I/O
+ * Date: 2026-02-17 23:37
+ */
+
+#include <iostream>
+#include <cstdio> // for using c functions like printf and scanf in c++ //
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
+
