87845c02ce3150a10e25bc9c422a12a17381c036
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-17 23:54:15 +0530

    ✅ Solved: Read & Print Integer (I/O)

---
+++ 01_Basics_IO/003_Read_Print_Integer.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Read & Print Integer
+ * Description: Read an integer from the user using cin and print it back using cout on a new line.
+ * Topic: I/O
+ * Date: 2026-02-17 23:54
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    cout << i << endl;
+    return 0;
+}
