a59d722931cc3b20fa18b178943bf61f2932b0b2
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-19 23:14:22 +0530

    ✅ Solved: Read Full Line (I/O)

---
+++ 01_Basics_IO/007_Read_Full_Line.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Read Full Line
+ * Description: Read a full line of text (including spaces) and print it back.
+ * Topic: I/O
+ * Date: 2026-02-19 23:14
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string myString;
+
+    // Use getline to read the entire line from cin into myString
+    getline(cin, myString);
+    cout << myString << endl;
+
+    return 0;
+}
