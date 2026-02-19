7adc8415eefa190352a7474f266dc0b345d999a8
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 01:10:46 +0530

    ✅ Solved: For Loop (For Loop)

---
+++ 03_Loops_Patterns/001_For_Loop.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem: For Loop
+ * Description: ? Solve ?
+ * Topic: For Loop
+ * Date: 2026-02-20 01:10
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    // Word mapping for numbers 1-9
+    vector<string> words = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            // Print the word for numbers 1 to 9
+            cout << words[i] << endl;
+        } 
+        else if (i > 9) {
+            // Check if the number is even or odd using modulo
+            if (i % 2 == 0) {
+                cout << "even" << endl;
+            } else {
+                cout << "odd" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
