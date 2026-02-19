af51ccf035c5c44291add25e40462bced5c579c8
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 02:21:53 +0530

    ✅ Solved: Print Odd Numbers (For Loop)

---
+++ 03_Loops_Patterns/005_Print_Odd_Numbers.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: Print Odd Numbers
+ * Description: Read N. Print all odd numbers from 1 to N.
+ * Topic: For Loop
+ * Date: 2026-02-20 02:21
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    for(int i=1;i<=N;i+=2)
+        cout << i << endl;
+}
