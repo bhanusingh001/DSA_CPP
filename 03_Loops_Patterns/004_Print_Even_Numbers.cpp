6d9c51541c638b81659887513ee510e1538dd646
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 01:23:28 +0530

    ✅ Solved: Print Even Numbers (For Loop)

---
+++ 03_Loops_Patterns/004_Print_Even_Numbers.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: Print Even Numbers
+ * Description: Read N. Print all even numbers from 1 to N.
+ * Topic: For Loop
+ * Date: 2026-02-20 01:23
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    for(int i=2;i<=N;i+=2)
+        cout << i << endl;
+}
