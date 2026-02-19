18abb55104b90699b0f524948cf4c9985711a8cf
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 01:16:37 +0530

    ✅ Solved: Print N to 1 (For Loop)

---
+++ 03_Loops_Patterns/003_Print_N_to_1.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: Print N to 1
+ * Description: Read N. Print all numbers from N down to 1.
+ * Topic: For Loop
+ * Date: 2026-02-20 01:16
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    for(int i=N;i>=1;i--)
+        cout << i << endl;
+}
