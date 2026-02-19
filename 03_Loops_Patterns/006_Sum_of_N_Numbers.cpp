7361a9853c6c4fab370b7d8ba63f12b9f3744c86
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 03:03:58 +0530

    ✅ Solved: Sum of N Numbers (For Loop)

---
+++ 03_Loops_Patterns/006_Sum_of_N_Numbers.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem: Sum of N Numbers
+ * Description: Read N. Print sum of 1+2+3+...+N using a for loop.
+ * Topic: For Loop
+ * Date: 2026-02-20 03:03
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N, sum=0;
+    cin >> N;
+    for(int i=1;i<=N;i++)
+        sum += i;
+    cout << sum;
+}
