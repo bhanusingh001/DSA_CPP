90b8ef80536e2cde1815fa6159e9fb3cbe824668
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 04:34:54 +0530

    ✅ Solved: Size of Data Types (Data Types)

---
+++ 01_Basics_IO/011_Size_of_Data_Types.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Size of Data Types
+ * Description: Print the size in bytes of int, float, double, char, bool, and long long using sizeof().
+ * Topic: Data Types
+ * Date: 2026-02-20 04:34
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of bool: " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of long long: " << sizeof(long long) << " bytes" << endl;
+
+    return 0;
+}
