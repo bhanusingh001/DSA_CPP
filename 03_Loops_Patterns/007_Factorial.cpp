7b9ca294c9e3dcbd40ef17e83130bfa467bdb649
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-20 03:12:35 +0530

    ✅ Solved: Factorial (For Loop)

---
+++ 03_Loops_Patterns/007_Factorial.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Factorial
+ * Description: Read N. Calculate and print N! (N factorial) using a for loop.
+ * Topic: For Loop
+ * Date: 2026-02-20 03:12
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    long long fact=1;
+    cin >> N;
+    for(int i=1;i<=N;i++)
+        fact *= i;
+    cout << fact;
+}
