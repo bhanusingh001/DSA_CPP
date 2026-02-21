15f15c20493fadf4b99b00fa264182b6d42776e9
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-21 23:49:38 +0530

    ✅ Solved: Even or Odd (Conditionals)

---
+++ 02_Operators_Conditionals/002_Even_or_Odd.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Even or Odd
+ * Description: ? Solve ?
+ * Topic: Conditionals
+ * Date: 2026-02-21 23:49
+ */
+
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+        if(n % 2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
