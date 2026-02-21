ef43b715f1b05641feac214b60c1e499b36c1699
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-21 23:54:31 +0530

    ✅ Solved: Count Digits (For Loop)

---
+++ 03_Loops_Patterns/012_Count_Digits.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem: Count Digits
+ * Description: Read a number. Count how many digits it has using a loop.
+ * Topic: For Loop
+ * Date: 2026-02-21 23:54
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    if(n == 0)
+        cnt = 1;
+    else {
+        n = abs(n);
+        while(n > 0){
+            n /= 10;
+            cnt++;
+        }
+    }
+
+    cout << "Digits = " << cnt;
+}
