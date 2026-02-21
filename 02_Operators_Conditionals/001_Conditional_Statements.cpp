764a6c56de0ff824c2f94b2912442957113dc47a
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-21 23:43:44 +0530

    ✅ Solved: Conditional Statements (Conditionals)

---
+++ 02_Operators_Conditionals/001_Conditional_Statements.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem: Conditional Statements
+ * Description: ? Solve ?
+ * Topic: Conditionals
+ * Date: 2026-02-21 23:43
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+  int n;
+    cin >> n;
+
+    // vector/array to store the words for easy mapping
+    // Index 0 is empty because we are dealing with 1-based numbers
+    vector<string> words = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    if (n >= 1 && n <= 9) {
+        cout << words[n] << endl;
+    } else {
+        cout << "Greater than 9" << endl;
+    }
+
+    return 0;
+}
