968bacf9f03e5dd92d50c6c95305427721357b08
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-21 23:56:56 +0530

    ✅ Solved: Reverse a Number (For Loop)

---
+++ 03_Loops_Patterns/014_Reverse_a_Number.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Reverse a Number
+ * Description: Read a number. Print it reversed. Example: 1234 ? 4321.
+ * Topic: For Loop
+ * Date: 2026-02-21 23:56
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0;
+    cin >> n;
+
+    int sign = (n < 0) ? -1 : 1; // store sign
+    n = abs(n);                  // make positive
+
+    while(n > 0){
+        rev = rev * 10 + n % 10; // add last digit
+        n /= 10;                 // remove last digit
+    }
+
+    cout << rev * sign;
+}
