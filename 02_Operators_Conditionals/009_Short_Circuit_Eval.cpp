dd94ecbbfc13b010e9d5ba5b39a539f64ffe70c6
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:44:01 +0530

    ✅ Solved: Short Circuit Eval (Operators)

---
+++ 02_Operators_Conditionals/009_Short_Circuit_Eval.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Short Circuit Eval
+ * Description: Write code to prove short-circuit: in (false && func()), func() never executes. Print proof.
+ * Topic: Operators
+ * Date: 2026-02-22 23:44
+ */
+
+#include <iostream>
+using namespace std;
+
+bool func(){
+    cout << "func() executed\n";
+    return true;
+}
+
+int main(){
+
+    cout << "Testing &&\n";
+    if(false && func())
+        cout << "Inside if\n";
+    else
+        cout << "Skipped func() in &&\n";
+
+
+    cout << "\nTesting ||\n";
+    if(true || func())
+        cout << "Skipped func() in ||\n";
+}
