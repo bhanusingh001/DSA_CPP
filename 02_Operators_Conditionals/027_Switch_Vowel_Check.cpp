f7a57b57fe3705c93ea948504f438d9ec031b130
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:59:56 +0530

    ✅ Solved: Switch: Vowel Check (Switch)

---
+++ 02_Operators_Conditionals/027_Switch_Vowel_Check.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: Switch: Vowel Check
+ * Description: Read a character. Check vowel/consonant using switch-case (handle both upper and lowercase).
+ * Topic: Switch
+ * Date: 2026-02-22 23:59
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cin >> ch;
+
+    switch(ch){
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            cout<<"Vowel";
+            break;
+        default:
+            cout<<"Consonant";
+    }
+}
