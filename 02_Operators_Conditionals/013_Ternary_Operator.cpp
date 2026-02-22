af388b9acb7aacf1fc72df431177563296844d29
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:48:18 +0530

    ✅ Solved: Ternary Operator (Operators)

---
+++ 02_Operators_Conditionals/013_Ternary_Operator.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Ternary Operator
+ * Description: Find max of two numbers using ternary (?:). Then find max of three using nested ternary.
+ * Topic: Operators
+ * Date: 2026-02-22 23:48
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    int max2 = (a>b)?a:b;
+    cout << "Max of two: " << max2 << endl;
+
+    int max3 = (a>b)? (a>c?a:c) : (b>c?b:c);
+    cout << "Max of three: " << max3;
+}
