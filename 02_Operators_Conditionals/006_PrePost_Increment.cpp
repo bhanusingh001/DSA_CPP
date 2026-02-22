f9584bbdb5f15913dd0eab39db9ed8beed0a6697
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:36:55 +0530

    ✅ Solved: Pre/Post Increment (Operators)

---
+++ 02_Operators_Conditionals/006_PrePost_Increment.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Pre/Post Increment
+ * Description: Given int b=5. Compute a=++b and a=b++. Print a and b after each. Explain difference.
+ * Topic: Operators
+ * Date: 2026-02-22 23:36
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    b=5;
+    a=++b;  // prefix increment - first b is incremented(6) then assigned to a(6)
+    cout << "a=" << a << " " << "b=" << b << endl;
+
+    b=5; 
+    a=b++; // Post increment - first b is assigned to a (a=5) then b is incremented(b=6)
+    cout << "a=" << a<< " " << "b=" << b << endl;
+}
