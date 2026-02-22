ca25e63629020e58cd0e40e65a690449e479a68b
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:57:37 +0530

    ✅ Solved: Triangle Validity (Conditionals)

---
+++ 02_Operators_Conditionals/022_Triangle_Validity.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Triangle Validity
+ * Description: Read 3 side lengths. Check if they form a valid triangle (sum of any 2 sides > third).
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:57
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a+b>c && a+c>b && b+c>a)
+        cout << "Valid Triangle";
+    else
+        cout << "Not Valid";
+}
