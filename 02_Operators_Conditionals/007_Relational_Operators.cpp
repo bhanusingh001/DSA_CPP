ce4f72f94fea5fc2669435c5c2536a92f9e9b5d1
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:38:24 +0530

    ✅ Solved: Relational Operators (Operators)

---
+++ 02_Operators_Conditionals/007_Relational_Operators.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem: Relational Operators
+ * Description: Read two numbers. Print results of ==, !=, <, >, <=, >= comparisons as true/false.
+ * Topic: Operators
+ * Date: 2026-02-22 23:38
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin >> x >> y;
+
+    cout << boolalpha;
+    cout << (x==y) << endl;
+    cout << (x!=y) << endl;
+    cout << (x<y) << endl;
+    cout << (x>y) << endl;
+    cout << (x<=y) << endl;
+    cout << (x>=y) << endl;
+}
