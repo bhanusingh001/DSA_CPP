a13dcb20f79e1f414911270b66672e88af20cce5
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:53:39 +0530

    ✅ Solved: Max of Two (Conditionals)

---
+++ 02_Operators_Conditionals/016_Max_of_Two.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Max of Two
+ * Description: Read two integers. Print the larger one without using max() function.
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:53
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+
+    if(a>b)
+        cout << a;
+    else
+        cout << b;
+}
