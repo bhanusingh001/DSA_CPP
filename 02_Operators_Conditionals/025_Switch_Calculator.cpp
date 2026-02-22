81ff16271d0131bae140cafaf2058c436ff404e6
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:59:12 +0530

    ✅ Solved: Switch: Calculator (Switch)

---
+++ 02_Operators_Conditionals/025_Switch_Calculator.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Switch: Calculator
+ * Description: Build calculator using switch on char operator. Handle +,-,*,/,% with default for invalid.
+ * Topic: Switch
+ * Date: 2026-02-22 23:59
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    char op;
+    cin >> a >> op >> b;
+
+    switch(op){
+        case '+': cout<<a+b; break;
+        case '-': cout<<a-b; break;
+        case '*': cout<<a*b; break;
+        case '/': cout<<a/b; break;
+        case '%': cout<<a%b; break;
+        default: cout<<"Invalid Operator";
+    }
+}
