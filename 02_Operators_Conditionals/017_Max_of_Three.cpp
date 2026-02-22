996f471419d6a4436c45e0ab06617d82eb1ce3ef
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:54:33 +0530

    ✅ Solved: Max of Three (Conditionals)

---
+++ 02_Operators_Conditionals/017_Max_of_Three.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem: Max of Three
+ * Description: Read three integers. Print the largest using nested if-else (no max function).
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:54
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a>b){
+        if(a>c)
+            cout << a;
+        else
+            cout << c;
+    }
+    else{
+        if(b>c)
+            cout << b;
+        else
+            cout << c;
+    }
+}
