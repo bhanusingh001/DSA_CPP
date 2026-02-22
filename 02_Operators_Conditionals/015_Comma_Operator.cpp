399ee3b68f205a3ee6793b96be20293f041d7723
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:52:48 +0530

    ✅ Solved: Comma Operator (Operators)

---
+++ 02_Operators_Conditionals/015_Comma_Operator.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: Comma Operator
+ * Description: What is the value of: int a = (5, 10, 15); ? Explain the comma operator.
+ * Topic: Operators
+ * Date: 2026-02-22 23:52
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = (5, 10, 15);
+    cout << a;
+    cout<< "\n" <<  "Comma operator evaluates left ? right but returns last value. ";    
+}
