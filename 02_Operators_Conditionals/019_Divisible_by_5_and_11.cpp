e11c007ed7771d10c4e4cfc6ea7e20c8180dfff3
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:56:22 +0530

    ✅ Solved: Divisible by 5 and 11 (Conditionals)

---
+++ 02_Operators_Conditionals/019_Divisible_by_5_and_11.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Divisible by 5 and 11
+ * Description: Read a number. Check if divisible by both 5 AND 11. Print Yes/No.
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:56
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n%5==0 && n%11==0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
