dbb04a2be8e25c1604bd883117d8fab00a797fac
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:27:23 +0530

    ✅ Solved: Arithmetic Operators (Operators)

---
+++ 02_Operators_Conditionals/004_Arithmetic_Operators.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Arithmetic Operators
+ * Description: Given two integers a, b. Print a+b, a-b, a*b, a/b, a%b. Note: 7/2 gives 3 (integer division).
+ * Topic: Operators
+ * Date: 2026-02-22 23:27
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+}
