5d6b8e240098f895365aac4eca0acd31d93822cb
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:45:55 +0530

    ✅ Solved: Left & Right Shift (Operators)

---
+++ 02_Operators_Conditionals/011_Left_Right_Shift.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem: Left & Right Shift
+ * Description: Multiply a number by 2 using <<. Divide by 2 using >>. Print original and results.
+ * Topic: Operators
+ * Date: 2026-02-22 23:45
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << "Original: " << n << endl;
+    cout << "Multiply by 2: " << (n<<1) << endl;
+    cout << "Divide by 2: " << (n>>1) << endl;
+}
