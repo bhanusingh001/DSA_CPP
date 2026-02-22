30a8d31fb015039a4f6622c392e3e9d33d1bbe4a
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:57:55 +0530

    ✅ Solved: Triangle Type (Conditionals)

---
+++ 02_Operators_Conditionals/023_Triangle_Type.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Triangle Type
+ * Description: Read 3 sides of a valid triangle. Classify: equilateral, isosceles, or scalene.
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:57
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a==b && b==c)
+        cout << "Equilateral";
+    else if(a==b || b==c || a==c)
+        cout << "Isosceles";
+    else
+        cout << "Scalene";
+}
