5141dea2697c8847d9a02a90194fd994fbd06d7c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-22 23:56:48 +0530

    ✅ Solved: Check Char Type (Conditionals)

---
+++ 02_Operators_Conditionals/020_Check_Char_Type.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: Check Char Type
+ * Description: Read a character. Print if it is an uppercase letter, lowercase letter, digit, or special character.
+ * Topic: Conditionals
+ * Date: 2026-02-22 23:56
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cin >> ch;
+
+    if(ch>='A' && ch<='Z')
+        cout << "Uppercase";
+    else if(ch>='a' && ch<='z')
+        cout << "Lowercase";
+    else if(ch>='0' && ch<='9')
+        cout << "Digit";
+    else
+        cout << "Special Character";
+}
