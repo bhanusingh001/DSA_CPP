007ad24508cf17db9fba9765526ae6b2c3c23273
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-23 23:52:58 +0530

    ✅ Solved: Century Year Check (Conditionals)

---
+++ 02_Operators_Conditionals/036_Century_Year_Check.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Century Year Check
+ * Description: Read a year. Check if it is a century year (divisible by 100). Print Yes/No.
+ * Topic: Conditionals
+ * Date: 2026-02-23 23:52
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cin>>year;
+
+    if(year%100==0)
+        cout<<"Yes";
+    else
+        cout<<"No";
+}
