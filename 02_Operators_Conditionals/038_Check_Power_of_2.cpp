1c5226e533767ab5a5ae17a83dc169296bc9ea9b
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-23 23:53:50 +0530

    ✅ Solved: Check Power of 2 (Conditionals)

---
+++ 02_Operators_Conditionals/038_Check_Power_of_2.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Check Power of 2
+ * Description: Read a positive integer. Check if it is a power of 2 (1,2,4,8,16...).
+ * Topic: Conditionals
+ * Date: 2026-02-23 23:53
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n>0 && (n&(n-1))==0)
+        cout<<"Yes";
+    else
+        cout<<"No";
+}
