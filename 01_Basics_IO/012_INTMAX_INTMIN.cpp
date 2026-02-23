68015f06f93b0e6cd6965f0604acf8fa3d4793d8
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-23 23:40:49 +0530

    ✅ Solved: INT_MAX & INT_MIN (Data Types)

---
+++ 01_Basics_IO/012_INTMAX_INTMIN.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: INT_MAX & INT_MIN
+ * Description: Print the maximum and minimum values that int and long long can hold.
+ * Topic: Data Types
+ * Date: 2026-02-23 23:40
+ */
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    cout << "int max: " << INT_MAX << endl;
+    cout << "int min: " << INT_MIN << endl;
+    cout << "long long max: " << LLONG_MAX << endl;
+    cout << "long long min: " << LLONG_MIN << endl;
+}
