d850fa928cc963f28fa00a6258716b43ffbe659f
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-23 23:43:15 +0530

    ✅ Solved: Unsigned Integer (Data Types)

---
+++ 01_Basics_IO/015_Unsigned_Integer.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem: Unsigned Integer
+ * Description: Store -1 in an unsigned int variable. Print the result and explain why.
+ * Topic: Data Types
+ * Date: 2026-02-23 23:43
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    unsigned int x = -1;
+    cout << x << endl;
+}
