417874da666aef54d947b6e0d60d9cf328b7291c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-23 23:52:32 +0530

    ✅ Solved: Profit or Loss (Conditionals)

---
+++ 02_Operators_Conditionals/035_Profit_or_Loss.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem: Profit or Loss
+ * Description: Read cost price and selling price. Print Profit/Loss amount or 'No Profit No Loss'.
+ * Topic: Conditionals
+ * Date: 2026-02-23 23:52
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    double cp,sp;
+    cin>>cp>>sp;
+
+    if(sp>cp) cout<<"Profit "<<sp-cp;
+    else if(cp>sp) cout<<"Loss "<<cp-sp;
+    else cout<<"No Profit No Loss";
+}
