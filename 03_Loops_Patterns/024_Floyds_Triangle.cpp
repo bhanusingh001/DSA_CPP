4da20f8d19584602b4604d1b0d6d3e1fab80d335
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:53:38 +0530

    ✅ Solved: Floyd's Triangle (Patterns)

---
+++ 03_Loops_Patterns/024_Floyds_Triangle.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Floyd's Triangle
+ * Description: Read N. Print Floyd triangle: 1\n2 3\n4 5 6\n... Sequential numbers.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:53
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int num = 1;
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
