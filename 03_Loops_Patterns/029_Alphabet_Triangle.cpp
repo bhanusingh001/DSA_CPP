40c8c083c859390fc27839c43360e905d526175c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:58:41 +0530

    ✅ Solved: Alphabet Triangle (Patterns)

---
+++ 03_Loops_Patterns/029_Alphabet_Triangle.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Alphabet Triangle
+ * Description: Read N. Print: A\nAB\nABC\nABCD...
+ * Topic: Patterns
+ * Date: 2026-02-24 23:58
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+
+    for(int i=1;i<=N;i++){
+        for(char ch='A'; ch<'A'+i; ch++)
+            cout<<ch;
+        cout<<endl;
+    }
+}
