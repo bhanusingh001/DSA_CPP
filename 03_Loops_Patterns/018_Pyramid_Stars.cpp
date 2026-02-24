f71c4c96de2b167ee1b883f75e842836e43b8d38
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:51:12 +0530

    ✅ Solved: Pyramid Stars (Patterns)

---
+++ 03_Loops_Patterns/018_Pyramid_Stars.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Pyramid Stars
+ * Description: Read N. Print centered pyramid of * with N rows. Use spaces for alignment.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:51
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        for(int space = 1; space <= N - i; space++)
+            cout << " ";
+
+        for(int star = 1; star <= 2*i - 1; star++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
