f64d2e66386dd95ed5c66797f912f96a9e8130d8
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:50:17 +0530

    ✅ Solved: Right Triangle Stars (Patterns)

---
+++ 03_Loops_Patterns/016_Right_Triangle_Stars.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Right Triangle Stars
+ * Description: Read N. Print right-angled triangle: row i has i stars. E.g., N=3: *\n**\n***
+ * Topic: Patterns
+ * Date: 2026-02-24 23:50
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
