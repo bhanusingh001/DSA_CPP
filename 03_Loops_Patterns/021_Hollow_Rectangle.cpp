b61415e8270639a3e66272f8daf3ff0471f9ca74
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:52:33 +0530

    ✅ Solved: Hollow Rectangle (Patterns)

---
+++ 03_Loops_Patterns/021_Hollow_Rectangle.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Hollow Rectangle
+ * Description: Read N rows, M cols. Print rectangle of * with hollow inside (only borders).
+ * Topic: Patterns
+ * Date: 2026-02-24 23:52
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= M; j++) {
+            if(i == 1 || i == N || j == 1 || j == M)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
