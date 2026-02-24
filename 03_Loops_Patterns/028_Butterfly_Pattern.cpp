31f618b1bd06d11f00008424bf186b2a0f41cae8
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:57:40 +0530

    ✅ Solved: Butterfly Pattern (Patterns)

---
+++ 03_Loops_Patterns/028_Butterfly_Pattern.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Butterfly Pattern
+ * Description: Read N. Print butterfly: two right triangles facing each other joined at widest.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:57
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=i;j++) cout<<"*";
+        for(int s=1;s<=2*(N-i);s++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<"*";
+        cout<<endl;
+    }
+
+    for(int i=N;i>=1;i--){
+        for(int j=1;j<=i;j++) cout<<"*";
+        for(int s=1;s<=2*(N-i);s++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<"*";
+        cout<<endl;
+    }
+}
