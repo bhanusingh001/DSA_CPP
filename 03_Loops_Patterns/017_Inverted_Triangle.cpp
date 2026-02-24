ccac3d0ccdca9032573068815110a0c6256c25ed
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:50:41 +0530

    ✅ Solved: Inverted Triangle (Patterns)

---
+++ 03_Loops_Patterns/017_Inverted_Triangle.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Inverted Triangle
+ * Description: Read N. Print inverted triangle: row 1 has N stars, row N has 1 star.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:50
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = N; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
