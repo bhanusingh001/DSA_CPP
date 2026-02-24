0bb9ec5ebd15465275dea8d80fabe7fa8b6f8b0d
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:59:56 +0530

    ✅ Solved: Reverse Alphabet Triangle (Patterns)

---
+++ 03_Loops_Patterns/031_Reverse_Alphabet_Triangle.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Reverse Alphabet Triangle
+ * Description: Read N. Print: ABCD\nABC\nAB\nA.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:59
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+
+    for(int i=N;i>=1;i--){
+        for(char ch='A'; ch<'A'+i; ch++)
+            cout<<ch;
+        cout<<endl;
+    }
+}
