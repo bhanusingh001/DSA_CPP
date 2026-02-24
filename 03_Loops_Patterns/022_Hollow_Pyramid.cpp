d61dd4efe9d3d52a21b8bb40621510d83717edeb
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-24 23:52:54 +0530

    ✅ Solved: Hollow Pyramid (Patterns)

---
+++ 03_Loops_Patterns/022_Hollow_Pyramid.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Hollow Pyramid
+ * Description: Read N. Print pyramid outline only ? stars at edges, spaces inside.
+ * Topic: Patterns
+ * Date: 2026-02-24 23:52
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        for(int space = 1; space <= N - i; space++)
+            cout << " ";
+
+        for(int j = 1; j <= 2*i - 1; j++) {
+            if(j == 1 || j == 2*i - 1 || i == N)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+}
