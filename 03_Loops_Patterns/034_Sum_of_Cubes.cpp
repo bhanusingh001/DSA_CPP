1fc62d56124098549318590dc57cdc6189129b10
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-25 23:51:34 +0530

    ✅ Solved: Sum of Cubes (For Loop)

---
+++ 03_Loops_Patterns/034_Sum_of_Cubes.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem: Sum of Cubes
+ * Description: Read N. Calculate 1? + 2? + 3? + ... + N?.
+ * Topic: For Loop
+ * Date: 2026-02-25 23:51
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    long long sum = 0;
+
+    for(int i = 1; i <= N; i++) {
+        sum += i * i * i;
+    }
+
+    cout << sum;
+}
