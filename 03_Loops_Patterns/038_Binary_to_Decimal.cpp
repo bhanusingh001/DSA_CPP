e40d12e11cf710dc84362eb9369818603c571518
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-25 23:53:05 +0530

    ✅ Solved: Binary to Decimal (For Loop)

---
+++ 03_Loops_Patterns/038_Binary_to_Decimal.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem: Binary to Decimal
+ * Description: Read a binary number (e.g., 1101). Convert and print its decimal value.
+ * Topic: For Loop
+ * Date: 2026-02-25 23:53
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int binary;
+    cin >> binary;
+
+    int decimal = 0, power = 0;
+
+    while(binary > 0) {
+        int lastDigit = binary % 10;
+        decimal += lastDigit * pow(2, power);
+        power++;
+        binary /= 10;
+    }
+
+    cout << decimal;
+}
