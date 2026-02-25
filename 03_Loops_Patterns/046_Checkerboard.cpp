a6b6159b5f2fb55b81ccac59f3442a492210782c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 00:00:56 +0530

    ✅ Solved: Checkerboard (Patterns)

---
+++ 03_Loops_Patterns/046_Checkerboard.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Checkerboard
+ * Description: Read N. Print N?N checkerboard: alternating * and spaces.
+ * Topic: Patterns
+ * Date: 2026-02-26 00:00
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if((i + j) % 2 == 0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
