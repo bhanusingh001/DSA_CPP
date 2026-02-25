043ca90b82db107789b2032ad46d52e107f2e299
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-25 23:54:32 +0530

    ✅ Solved: Print Divisors (For Loop)

---
+++ 03_Loops_Patterns/042_Print_Divisors.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Print Divisors
+ * Description: Read N. Print all divisors of N in ascending order.
+ * Topic: For Loop
+ * Date: 2026-02-25 23:54
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        if(N % i == 0)
+            cout << i << " ";
+    }
+}
