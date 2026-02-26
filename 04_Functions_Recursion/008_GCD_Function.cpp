debe358fede12b2118f946c415d72a3f38ff0000
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:44:55 +0530

    ✅ Solved: GCD Function (Functions)

---
+++ 04_Functions_Recursion/008_GCD_Function.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem: GCD Function
+ * Description: Write int gcd(int a, int b) using Euclidean algorithm. Test with (48,18) ? 6.
+ * Topic: Functions
+ * Date: 2026-02-26 23:44
+ */
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    cout << gcd(48,18) << endl;
+}
