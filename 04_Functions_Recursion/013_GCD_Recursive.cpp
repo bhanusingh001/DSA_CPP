d410c9534171445cf5bf6b46c472ab111a2cd205
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:47:58 +0530

    ✅ Solved: GCD Recursive (Recursion)

---
+++ 04_Functions_Recursion/013_GCD_Recursive.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: GCD Recursive
+ * Description: Write int gcd(int a,int b) recursively. Base: gcd(a,0)=a. Recursive: gcd(b,a%b).
+ * Topic: Recursion
+ * Date: 2026-02-26 23:47
+ */
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+int main() {
+    cout << gcd(48, 18);
+}
