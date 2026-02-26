1b8b43b70e68e6667fb58c22decaedfeff527cb6
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:43:32 +0530

    ✅ Solved: Inline Function (Functions)

---
+++ 04_Functions_Recursion/005_Inline_Function.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem: Inline Function
+ * Description: Write inline int square(int x){return x*x;} Explain when to use inline.
+ * Topic: Functions
+ * Date: 2026-02-26 23:43
+ */
+
+inline int square(int x) {
+    return x * x;
+}
