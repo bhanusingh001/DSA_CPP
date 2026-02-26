2dcfadc552ef6721baffbd5dad4c6c6e8a82f4bd
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:46:40 +0530

    ✅ Solved: Reverse String Recursive (Recursion)

---
+++ 04_Functions_Recursion/011_Reverse_String_Recursive.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: Reverse String Recursive
+ * Description: Reverse a string using recursion. No loops allowed.
+ * Topic: Recursion
+ * Date: 2026-02-26 23:46
+ */
+
+#include <iostream>
+using namespace std;
+
+void reverseString(string s) {
+    if(s.length() == 0)
+        return;
+
+    reverseString(s.substr(1));  // recurse without first char
+    cout << s[0];                // print first char later
+}
+
+int main() {
+    string str;
+    cin >> str;
+    reverseString(str);
+}
