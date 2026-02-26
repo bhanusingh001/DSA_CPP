a4ac74d63aa1d7042d95ce2a9b7a0581f094e165
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:41:58 +0530

    ✅ Solved: Function Overloading 1 (Functions)

---
+++ 04_Functions_Recursion/003_Function_Overloading_1.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem: Function Overloading 1
+ * Description: Overload area(): area(r) for circle, area(l,b) for rectangle, area(b,h,0.5) for triangle.
+ * Topic: Functions
+ * Date: 2026-02-26 23:41
+ */
+
+#include <iostream>
+using namespace std;
+
+double area(double r) {
+    return 3.14159 * r * r;   // Circle
+}
+
+double area(double l, double b) {
+    return l * b;             // Rectangle
+}
+
+double area(double b, double h, double half) {
+    return half * b * h;      // Triangle
+}
+
+int main() {
+    cout << "Circle: " << area(5) << endl;
+    cout << "Rectangle: " << area(4,6) << endl;
+    cout << "Triangle: " << area(4,6,0.5) << endl;
+}
