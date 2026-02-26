baece75bd4bd75f8359434b67df11d001577ee81
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-26 23:43:52 +0530

    ✅ Solved: Temperature Converter (Functions)

---
+++ 04_Functions_Recursion/006_Temperature_Converter.cpp
@@ -0,0 +1,24 @@
+/*
+ * Problem: Temperature Converter
+ * Description: Write celsiusToFahr(c) and fahrToCelsius(f). Test with 0?C, 100?C, 32?F, 212?F.
+ * Topic: Functions
+ * Date: 2026-02-26 23:43
+ */
+
+#include <iostream>
+using namespace std;
+
+double celsiusToFahr(double c) {
+    return (c * 9/5) + 32;
+}
+
+double fahrToCelsius(double f) {
+    return (f - 32) * 5/9;
+}
+
+int main() {
+    cout << "0?C = " << celsiusToFahr(0) << "?F" << endl;
+    cout << "100?C = " << celsiusToFahr(100) << "?F" << endl;
+    cout << "32?F = " << fahrToCelsius(32) << "?C" << endl;
+    cout << "212?F = " << fahrToCelsius(212) << "?C" << endl;
+}
