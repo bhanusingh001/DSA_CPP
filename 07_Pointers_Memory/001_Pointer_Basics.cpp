ab297fd7b8d636c1251bd8bcd006eda39d5ab6ab
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-27 21:39:45 +0530

    ✅ Solved: Pointer Basics (Pointers)

---
+++ 07_Pointers_Memory/001_Pointer_Basics.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Pointer Basics
+ * Description: Declare int a=42; int* p=&a;. Print a, *p (value at p), p (address), &a. Show they match.
+ * Topic: Pointers
+ * Date: 2026-02-27 21:39
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 42;
+    int* p = &a;
+
+    cout << "a = " << a << endl;
+    cout << "*p = " << *p << endl;
+    cout << "p (address) = " << p << endl;
+    cout << "&a (address) = " << &a << endl;
+
+    return 0;
+}
