e1cf75af233f02647a5a27531b73c4e61143d16b
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-27 23:37:41 +0530

    ✅ Solved: Pointer Arithmetic (Pointers)

---
+++ 07_Pointers_Memory/002_Pointer_Arithmetic.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Pointer Arithmetic
+ * Description: Create int arr[5]={10,20,30,40,50}; int* p=arr;. Print *(p+0), *(p+1), *(p+2). Explain pointer+1.
+ * Topic: Pointers
+ * Date: 2026-02-27 23:37
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10,20,30,40,50};
+    int* p = arr;   // same as &arr[0]
+
+    cout << *(p+0) << endl;
+    cout << *(p+1) << endl;
+    cout << *(p+2) << endl;
+
+    return 0;
+}
