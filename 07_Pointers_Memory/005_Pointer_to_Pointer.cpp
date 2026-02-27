ec543cb06000cceb1373bd084836ee98068b34a7
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-27 23:40:36 +0530

    ✅ Solved: Pointer to Pointer (Pointers)

---
+++ 07_Pointers_Memory/005_Pointer_to_Pointer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem: Pointer to Pointer
+ * Description: Declare int a=5; int* p=&a; int** pp=&p;. Print a, *p, **pp. Show all give same value.
+ * Topic: Pointers
+ * Date: 2026-02-27 23:40
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int* p = &a;
+    int** pp = &p;
+
+    cout << "a = " << a << endl;
+    cout << "*p = " << *p << endl;
+    cout << "**pp = " << **pp << endl;
+
+    return 0;
+}
