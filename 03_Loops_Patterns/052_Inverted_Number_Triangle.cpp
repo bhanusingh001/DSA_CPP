e5668687939181d708465db76eff7b817787dffd
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-02-28 23:59:53 +0530

    ✅ Solved: Inverted Number Triangle (Patterns)

---
+++ 03_Loops_Patterns/052_Inverted_Number_Triangle.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Inverted Number Triangle
+ * Description: Read N=5. Print: 54321\n4321\n321\n21\n1.
+ * Topic: Patterns
+ * Date: 2026-02-28 23:59
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N = 5;
+
+    for(int i = N; i >= 1; i--) {
+        for(int j = i; j >= 1; j--)
+            cout << j;
+        cout << endl;
+    }
+}
