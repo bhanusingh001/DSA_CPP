35723e51277f98f5c39a1b97982a92e0d58def3f
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-03-01 22:57:50 +0530

    ✅ Solved: GCD / HCF (For Loop)

---
+++ 03_Loops_Patterns/054_GCD_HCF.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem: GCD / HCF
+ * Description: ? Solve ?
+ * Topic: For Loop
+ * Date: 2026-03-01 22:57
+ */
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
