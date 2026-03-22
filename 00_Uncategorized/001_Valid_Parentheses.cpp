cf66769f3781f5bf62a32d443e36d5aaeae78b47
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-03-22 23:59:50 +0530

    ✅ Solved: Valid Parentheses (Stack & Queue)

---
+++ 00_Uncategorized/001_Valid_Parentheses.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Valid Parentheses
+ * Link: https://leetcode.com/problems/valid-parentheses/
+ * Topic: Stack & Queue
+ * Date: 2026-03-22 23:59
+ */
+
+class Solution {
+public:
+    bool isPair(char l, char r) {
+        return ((l == '(' && r == ')') || 
+                (l == '{' && r == '}') || 
+                (l == '[' && r == ']'));
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (!st.empty() && isPair(st.top(), c)) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
