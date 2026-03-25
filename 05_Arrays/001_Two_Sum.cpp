ffcc28582d6160f4667478ab3d45918207eddb8c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-03-25 23:40:53 +0530

    ✅ Solved: Two Sum (Arrays)

---
+++ 05_Arrays/001_Two_Sum.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem: Two Sum
+ * Description: ? Solve ?
+ * Topic: Arrays
+ * Date: 2026-03-25 23:40
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int k) {
+     unordered_map<int,int> m;
+           vector<int> ans;
+
+     for(int i=0;i<nums.size();i++){
+        int rem=k-nums[i];
+        if(m.find(rem)!=m.end()){
+            ans.push_back(i);
+            ans.push_back(m[rem]);
+        }
+        else m[nums[i]]=i;
+     }
+  
+        
+        
+       return ans; 
+    }
+    };
