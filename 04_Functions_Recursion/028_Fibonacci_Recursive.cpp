06d7b2778be82b31217ebd5418185fc956ccd5c5
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-03-26 23:46:45 +0530

    ✅ Solved: Fibonacci Recursive (Recursion)

---
+++ 04_Functions_Recursion/028_Fibonacci_Recursive.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem: Fibonacci Recursive
+ * Description: ? Solve ?
+ * Topic: Recursion
+ * Date: 2026-03-26 23:46
+ */
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
