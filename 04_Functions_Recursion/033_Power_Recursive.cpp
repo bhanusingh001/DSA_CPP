d43a4b23323e1fb4d3a2547506cdc3d1cf5d6182
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-01 23:45:19 +0530

    ✅ Solved: Power Recursive (Recursion)

---
+++ 04_Functions_Recursion/033_Power_Recursive.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem: Power Recursive
+ * Description: ? Solve ?
+ * Topic: Recursion
+ * Date: 2026-04-01 23:45
+ */
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n < 0) {
+            x = 1 / x;
+        } 
+        
+        long num = labs(n);
+        
+        double pow = 1;
+        
+        while(num){ // equivalent to while(num != 0)
+            if(num & 1) { // equivalent to if((num & 1) != 0)
+                pow *= x;
+            }
+            
+            x *= x;
+            num >>= 1;
+        }
+        
+        return pow;
+    }
+};
