ba501ec9d389aef61804859ba2f2dbe33f292870
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-02 23:49:08 +0530

    ✅ Solved: Contains Duplicate (Arrays)

---
+++ 05_Arrays/003_Contains_Duplicate.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Contains Duplicate
+ * Description: ? Solve ?
+ * Topic: Arrays
+ * Date: 2026-04-02 23:49
+ */
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num) > 0)
+                return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
