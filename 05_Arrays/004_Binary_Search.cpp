2bcc46da5d362c30536b5cea4fbc73c8692c3df1
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-03 22:45:42 +0530

    ✅ Solved: Binary Search (Arrays)

---
+++ 05_Arrays/004_Binary_Search.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Binary Search
+ * Description: ? Solve ?
+ * Topic: Arrays
+ * Date: 2026-04-03 22:45
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
