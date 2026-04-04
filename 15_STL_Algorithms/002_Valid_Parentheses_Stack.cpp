512d5d765d3f47a0612cb5d3c14b8cba8f8deebc
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-04 23:31:24 +0530

    ✅ Solved: Valid Parentheses Stack (STL-Stack)

---
+++ 15_STL_Algorithms/002_Valid_Parentheses_Stack.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem: Valid Parentheses Stack
+ * Description: ? Solve ?
+ * Topic: STL-Stack
+ * Date: 2026-04-04 23:31
+ */
+
+class Solution {
+public:
+    bool isPair(char l, char r) {
+        return ((l == '(' && r == ')') || 
+                (l == '{' && r == '}') || 
+                (l == '[' && r == ']'));
+    }
+
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (!st.empty() && isPair(st.top(), c)) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
