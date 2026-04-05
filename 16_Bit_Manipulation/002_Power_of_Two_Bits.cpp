db123a04c27b6991a93029576452a4d8f2b4b8cb
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-05 23:36:42 +0530

    ✅ Solved: Power of Two Bits (Bits)

---
+++ 16_Bit_Manipulation/002_Power_of_Two_Bits.cpp
@@ -0,0 +1,13 @@
+/*
+ * Problem: Power of Two Bits
+ * Description: ? Solve ?
+ * Topic: Bits
+ * Date: 2026-04-05 23:36
+ */
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
