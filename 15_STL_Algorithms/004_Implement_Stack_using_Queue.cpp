bbe3c237c6f7e9636eef9d5a3e98bae2f713bb59
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-07 23:53:54 +0530

    ✅ Solved: Implement Stack using Queue (STL-Queue)

---
+++ 15_STL_Algorithms/004_Implement_Stack_using_Queue.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: Implement Stack using Queue
+ * Description: ? Solve ?
+ * Topic: STL-Queue
+ * Date: 2026-04-07 23:53
+ */
+
+class MyStack {
+private:
+    std::queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
