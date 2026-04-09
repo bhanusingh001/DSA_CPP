11d3649baf5539af67c4b9a4836a588956c1ea34
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-09 23:50:58 +0530

    ✅ Solved: Valid Parentheses Map (STL-Map)

---
+++ 14_STL_Associative/001_Valid_Parentheses_Map.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem: Valid Parentheses Map
+ * Description: ? Solve ?
+ * Topic: STL-Map
+ * Date: 2026-04-09 23:50
+ */
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char val: s){
+            if(val=='(' || val=='{' || val=='['){
+                st.push(val);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if((st.top()=='(' && val==')') || (st.top()=='{' && val=='}') || (st.top()=='[' && val==']')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
