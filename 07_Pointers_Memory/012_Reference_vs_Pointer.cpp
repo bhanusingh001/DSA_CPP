2eb13b5d86b29c8dbb36bb54a31c6a0206057d5f
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-11 23:36:40 +0530

    ✅ Solved: Reference vs Pointer (Pointers)

---
+++ 07_Pointers_Memory/012_Reference_vs_Pointer.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem: Reference vs Pointer
+ * Description: Demonstrate differences: reference must init, can't be null, can't rebind. Pointer can do all.
+ * Topic: Pointers
+ * Date: 2026-04-11 23:36
+ */
+
+#include <iostream>
+
+int main() {
+    int a = 10, b = 20;
+
+    // REFERENCES
+    int& ref = a;    // 1. Must initialize immediately
+    // int& ref2;    // Error!
+    ref = b;         // 2. Can't rebind (this just sets a = 20)
+    // &ref = &b;    // Error!
+    
+    // POINTERS
+    int* ptr;        // 1. Can be uninitialized (though risky)
+    ptr = nullptr;   // 2. Can be null
+    ptr = &a;        // 3. Can rebind (points to a)
+    ptr = &b;        // Now points to b
+
+    return 0;
+}
