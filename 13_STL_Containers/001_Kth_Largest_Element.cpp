2241335e06be0300c630833f9256b901fd90ea78
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-14 23:13:33 +0530

    ✅ Solved: Kth Largest Element (STL-Vector)

---
+++ 13_STL_Containers/001_Kth_Largest_Element.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem: Kth Largest Element
+ * Description: ? Solve ?
+ * Topic: STL-Vector
+ * Date: 2026-04-14
+ */
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
