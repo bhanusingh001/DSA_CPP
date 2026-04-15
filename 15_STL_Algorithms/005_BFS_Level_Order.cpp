43ecb1b17b3205972751beb875676cd85a88cf06
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-15 22:26:47 +0530

    ✅ Solved: BFS Level Order (STL-Queue)

---
+++ 15_STL_Algorithms/005_BFS_Level_Order.cpp
@@ -0,0 +1,43 @@
+/*
+ * Problem: BFS Level Order
+ * Description: ? Solve ?
+ * Topic: STL-Queue
+ * Date: 2026-04-15
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void levelOrderQueue(TreeNode* root,vector<vector<int>>& ans){
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> v;
+            for(int i=0;i<size;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+            }
+            ans.push_back(v);
+        }
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        levelOrderQueue(root,ans);
+        return ans;
+    }
+};
