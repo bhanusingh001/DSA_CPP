7a63c4492cbdaab0f2906a995f97b39fdf0c88dc
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-17 23:36:53 +0530

    ✅ Solved: Check if Array is Sorted (C++ Basics & STL)

---
+++ 18_CPP_Basics_STL/001_Check_if_Array_is_Sorted.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Check if Array is Sorted
+ * Description: ? Solve ?
+ * Topic: C++ Basics & STL
+ * Date: 2026-04-17
+ */
+
+class Solution {
+  public:
+    bool isSorted(vector<int> &arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] >= arr[i - 1]) {
+                // ok
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};  
