041ff63b327093eb606196303cdfe945aa9a8dee
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-19 23:44:34 +0530

    ✅ Solved: Count of Matches in a Tournament (C++ Basics & STL)

---
+++ 18_CPP_Basics_STL/003_Count_of_Matches_in_a_Tournament.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem: Count of Matches in a Tournament
+ * Description: ? Solve ?
+ * Topic: C++ Basics & STL
+ * Date: 2026-04-19
+ */
+
+//Approach-1 (Doing simple simulation asking in code)
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int result = 0;
+        
+        while(n > 1) {
+            if(n%2 == 0) {
+                result += n/2;
+                n /= 2;
+            } else {
+                result += (n-1)/2;
+                n = ((n-1)/2) + 1;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (Using simple math)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
