cbad75f46892c89804c852a64faba62b60a8cf5c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-20 23:10:57 +0530

    ✅ Solved: Find the Difference (Bit Manipulation)

---
+++ 31_Bit_Manipulation/002_Find_the_Difference.cpp
@@ -0,0 +1,66 @@
+/*
+ * Problem: Find the Difference
+ * Description: ? Solve ?
+ * Topic: Bit Manipulation
+ * Date: 2026-04-20
+ */
+
+//Approach-1
+//T.C : Linear
+//S.C : Extra space map
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> mp;
+        
+        for(char &ch : s) {
+            mp[ch]++;
+        }
+        
+        for(char &ch : t) {
+            mp[ch]--;
+            
+            if(mp[ch] < 0)
+                return ch;
+        }
+        
+        return 'a'; //send any random. It will never reach here
+    }
+};
+
+//Approach-2 (Using Sum Difference)
+//T.C : Linear
+//S.C : constant
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        
+        for (char &ch: t)
+            sum += ch;
+        
+        for (char &ch: s)
+            sum -=ch;
+        
+        return (char)sum;
+    }
+};
+
+
+//Approach-3 (Using XOR property)
+//T.C : Linear
+//S.C : constant
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int XOR = 0;
+        
+        for (char &ch: t)
+            XOR ^= ch;
+        
+        for (char &ch: s)
+            XOR ^=ch;
+        
+        return (char)XOR;
+    }
+};
