aaaa5d56ead6b3e0c66bb03196218aefcaa10c2c
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-24 22:50:00 +0530

    ✅ Solved: Left Rotate Array by K (C++ Basics & STL)

---
+++ 18_CPP_Basics_STL/004_Left_Rotate_Array_by_K.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Left Rotate Array by K
+ * Description: ? Solve ?
+ * Topic: C++ Basics & STL
+ * Date: 2026-04-24
+ */
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
