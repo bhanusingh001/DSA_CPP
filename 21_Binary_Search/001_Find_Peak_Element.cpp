4474bb4474e014687d8095b1ec911ab2a5d432e0
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-04-27 22:55:47 +0530

    ✅ Solved: Find Peak Element (Binary Search)

---
+++ 21_Binary_Search/001_Find_Peak_Element.cpp
@@ -0,0 +1,25 @@
+/*
+ * Problem: Find Peak Element
+ * Description: ? Solve ?
+ * Topic: Binary Search
+ * Date: 2026-04-27
+ */
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;        
+    }
+};
