f6b5e1e99e4bd9192107f48ccee6d531bc5f56bd
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-05-01 23:50:42 +0530

    ✅ Solved: Intersection of Two Linked Lists (Linked List)

---
+++ 23_Linked_List/001_Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem: Intersection of Two Linked Lists
+ * Description: ? Solve ?
+ * Topic: Linked List
+ * Date: 2026-05-01
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/*Time Complexity : O(m + n)
+Space Complexity : O(1)
+*/
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
+	}
+};
