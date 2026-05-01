290903cba14f80234ef73d588842fdd738a20bd6
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-05-01 23:47:21 +0530

    ✅ Solved: Valid Palindrome (Strings)

---
+++ 20_Strings/001_Valid_Palindrome.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem: Valid Palindrome
+ * Description: ? Solve ?
+ * Topic: Strings
+ * Date: 2026-05-01
+ */
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    
+    string clean = "";
+    for(char c : s){
+            if(isalnum(c))
+                clean += tolower(c);
+        }
+        int i=0;
+    int j= clean.size()-1;
+
+    while(i<=j){
+        if(clean[i] == clean[j]){
+        i++;
+        j--;}
+        else 
+        return false;
+    }
+    return true;
+    }
+};
