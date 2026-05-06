0dedc02aae4457636fbb2110e151715c36860c77
Bhanu Singh <124198459+bhanusingh001@users.noreply.github.com>
2026-05-07 00:01:21 +0530

    ✅ Solved: Next Greater Element III (Stack & Queue)

---
+++ 24_Stack_Queue/004_Next_Greater_Element_III.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem: Next Greater Element III
+ * Description: ? Solve ?
+ * Topic: Stack & Queue
+ * Date: 2026-05-07
+ */
+
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        int m = s.size();
+
+        int pivot = -1;
+
+        for(int i = m - 2; i >= 0; i--) {
+            if(s[i] < s[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1) return -1;
+
+        int j = m - 1;
+        while(s[j] <= s[pivot]) j--;
+
+        swap(s[pivot], s[j]);
+
+        reverse(s.begin() + pivot + 1, s.end());
+
+        long long ans = stoll(s);
+        return (ans > INT_MAX) ? -1 : ans;
+    }
+};
